f00a5b1421399ea0ab7d3f2f603bd4eacfd0641d
hasunzo <gktjsdl470@gmail.com>
2021-10-08 16:51:55 +0900

    먼저 setUp 호출하기

--- src/2_xUit/study.py
+++ src/2_xUit/study.py
@@ -3,7 +3,11 @@ class TestCase:
     def __init__(self, name):
         self.name= name
 
+    def setUp(self):
+        pass
+
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
@@ -16,12 +20,21 @@ class WasRun(TestCase):
     def testMethod(self):
         self.wasRun= 1
 
+    def setUp(self):
+        self.wasRun= None
+        self.wasSetUp= 1
+
 ## TestCaseTest
 class TestCaseTest(TestCase):
     def testRunning(self):
         test= WasRun("testMethod")
-        assert (not test.wasRun)
         test.run()
         assert (test.wasRun)
 
+    def testSetUp(self):
+        test= WasRun("testMethod")
+        test.run()
+        assert (test.wasSetUp)
+
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
