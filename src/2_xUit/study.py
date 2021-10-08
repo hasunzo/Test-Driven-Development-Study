41cc5f8921505e010b498a916559e75812cf564b
hasunzo <gktjsdl470@gmail.com>
2021-10-08 16:32:48 +0900

    fix update study.py

--- src/2_xUit/study.py
+++ src/2_xUit/study.py
@@ -1,30 +1,27 @@
-##TestCase
+## TestCase
 class TestCase:
     def __init__(self, name):
-        self.name = name
+        self.name= name
 
     def run(self):
         method = getattr(self, self.name)
-        method();
+        method()
 
 ## WasRun
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun = None
+        self.wasRun= None
         TestCase.__init__(self, name)
+
     def testMethod(self):
-        self.wasRun = 1
+        self.wasRun= 1
 
-"""test= WasRun("testMethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)"""
-
-##TestCaseTest
+## TestCaseTest
 class TestCaseTest(TestCase):
     def testRunning(self):
-        test = WasRun("testMehtod")
-        assert(not test.wasRun)
+        test= WasRun("testMethod")
+        assert (not test.wasRun)
         test.run()
-        assert(test.wasRun)
-TestCaseTest("testMethod").run()
+        assert (test.wasRun)
+
+TestCaseTest("testRunning").run()
