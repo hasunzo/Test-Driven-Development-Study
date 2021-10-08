04fb02d9efbe3832accc8da2a20f6ca3ab50a846
hasunzo <gktjsdl470@gmail.com>
2021-10-08 14:59:04 +0900

    테스트 메서드 호출하기

--- src/2_xUit/study.py
+++ src/2_xUit/study.py
@@ -1,12 +1,30 @@
-
-
-class WasRun:
+##TestCase
+class TestCase:
     def __init__(self, name):
-        self.wasRun= None
-    def testMethod(self):
-        self.wasRun=1
+        self.name = name
 
-test = WasRun("testMethod")
-print (test.wasRun)
-test.testMethod()
-print (test.wasRun)
+    def run(self):
+        method = getattr(self, self.name)
+        method();
+
+## WasRun
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun = None
+        TestCase.__init__(self, name)
+    def testMethod(self):
+        self.wasRun = 1
+
+"""test= WasRun("testMethod")
+print(test.wasRun)
+test.run()
+print(test.wasRun)"""
+
+##TestCaseTest
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMehtod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
+TestCaseTest("testMethod").run()
