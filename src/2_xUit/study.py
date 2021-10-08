1b183057596c426d4e77790ab7613859e06ed056
hasunzo <gktjsdl470@gmail.com>
2021-10-08 18:29:20 +0900

    WasRun에 로그 문자열 남기기

--- src/2_xUit/study.py
+++ src/2_xUit/study.py
@@ -19,6 +19,7 @@ class WasRun(TestCase):
 
     def testMethod(self):
         self.wasRun= 1
+        self.log= self.log + "testMethod "
 
     def setUp(self):
         self.wasRun= None
@@ -30,13 +31,9 @@ class TestCaseTest(TestCase):
     def setUp(self):
         self.test= WasRun("testMethod")
 
-    def testRunning(self):
-        self.test.run()
-        assert (self.test.wasRun)
+    def testTemplateMethod(self):
+        test= WasRun("testMethod")
+        test.run()
+        assert ("setUp testMethod " == test.log)
 
-    def testSetUp(self):
-        self.test.run()
-        assert ("setUp " == self.test.log)
-
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
